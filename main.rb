0b4addb5422651d41782a3825530bb5a162cccc4
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-08-19 00:15:59 -0500

    feat: allow word of proper length to be pulled from included dictionary

--- main.rb
+++ main.rb
@@ -1,6 +1,30 @@
-dictionary = 'dictionary.txt'
-random_line = (0..61406).to_a.sample
-File.open(dictionary) do |file|
-  random_line.times { file.readline }
-  puts file.readline
+require "pry"
+
+DICTIONARY_FILE = "dictionary.txt"
+
+# Contains all game logic and methods for game functions
+class Game
+  attr_accessor :word
+
+  def initialize
+    @word = ""
+  end
+
+  def get_word
+    until long_word?
+      random_line = (0..61_406).to_a.sample
+      File.open(DICTIONARY_FILE) do |file|
+        random_line.times { file.readline }
+        self.word = file.readline
+      end
+    end
+  end
+
+  def long_word?
+    (5..12).include?(word.length)
+  end
 end
+
+hangman = Game.new
+hangman.get_word
+puts hangman.word
