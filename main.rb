c1a9e7b62a46223795f77ca246dc948b11c77c35
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-08-19 01:44:24 -0500

    feat(gameplay): add basic round play and loss detection

--- main.rb
+++ main.rb
@@ -1,16 +1,19 @@
-require "pry"
+require 'pry'
 
-DICTIONARY_FILE = "dictionary.txt"
+DICTIONARY_FILE = 'dictionary.txt'.freeze
 
-# Contains all game logic and methods for game functions
-class Game
+class Player
+  def initialize; end
+end
+
+class Computer < Player
   attr_accessor :word
 
-  def initialize
-    @word = ""
+  def initialize()
+    @word = ''
   end
 
-  def get_word
+  def set_word
     until long_word?
       random_line = (0..61_406).to_a.sample
       File.open(DICTIONARY_FILE) do |file|
@@ -25,6 +28,58 @@ class Game
   end
 end
 
-hangman = Game.new
-hangman.get_word
-puts hangman.word
+class Human < Player
+  attr_accessor :guesses, :chances_left, :current_guess
+
+  def initialize(*args)
+    @guesses = []
+    @chances_left = 10
+    @current_guess = ''
+  end
+
+  def get_guess
+    puts 'Please enter a letter'
+    self.current_guess = gets.chomp
+    guesses << current_guess
+  end
+
+  def bad_guess
+    self.chances_left -= 1
+  end
+end
+
+# Contains all game logic and methods for game functions
+class Game
+  attr_accessor :human, :computer
+
+  def initialize(human, computer)
+    @human = human
+    @computer = computer
+  end
+
+  def correct_guess?
+    computer.word.include?(human.current_guess)
+  end
+
+  def play_game
+    computer.set_word
+    loop do
+      puts computer.word
+      play_round
+      p human.guesses
+      puts human.chances_left
+      break if human.chances_left == 0
+    end
+  end
+
+  def play_round
+    human.get_guess
+    human.bad_guess unless correct_guess?
+  end
+end
+
+human = Human.new
+computer = Computer.new
+hangman = Game.new(human, computer)
+
+hangman.play_game
